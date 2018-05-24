faff6001ce64d0d98322145f2186dfc2bbafdd07
rotimi-babalola <rotimi.babalola@andela.com>
2018-05-24 13:14:29 +0100

    add controller for signin

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,7 +1,9 @@
 import { User } from '../models';
 import signToken from '../utils/signToken';
+import verifyPassword from '../utils/verifyPassword';
 
 class UserController {
+  // create user
   static create(request, response) {
     User.create(request.body)
       .then((newUser) => {
@@ -18,6 +20,39 @@ class UserController {
         });
       })
       .catch(error => response.status(500).send(error));
+  }
+
+  static signin(request, response) {
+    User.findOne({
+      where: {
+        email: request.body.email,
+      },
+    }).then((foundUser) => {
+      if (!foundUser) {
+        return response.status(404).send({
+          success: false,
+          message: 'User not found',
+        });
+      } else if (!verifyPassword(request.body.password, foundUser.password)) {
+        return response.status(401).send({
+          success: false,
+          message: 'Signin failed! Incorrect password',
+        });
+      }
+
+      const token = signToken(foundUser);
+      return response.status(200).send({
+        success: true,
+        message: 'Signin successful!',
+        token,
+        user: {
+          id: foundUser.id,
+          name: foundUser.name,
+          role: foundUser.role,
+          email: foundUser.email,
+        },
+      });
+    }).catch(error => response.status(500).send(error));
   }
 }
 
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import UsercController from '../controllers/user.controllers';
+import UserController from '../controllers/user.controllers';
 
 const router = express.Router();
 
 router.route('/')
-  .post(UsercController.create);
+  .post(UserController.create);
+
+router.route('/signin')
+  .post(UserController.signin);
 
 export default router;
