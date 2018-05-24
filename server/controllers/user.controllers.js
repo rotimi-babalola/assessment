38fcda543713f540b30479c608ba4482803ccacd
rotimi-babalola <rotimi.babalola@andela.com>
2018-05-24 12:59:09 +0100

    send token on signup

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,17 +1,22 @@
 import { User } from '../models';
+import signToken from '../utils/signToken';
 
 class UserController {
   static create(request, response) {
     User.create(request.body)
-      .then((newUser) => response.status(201).send({
-        success: true,
-        user: {
-          id: newUser.id,
-          name: newUser.name,
-          role: newUser.role,
-          email: newUser.email,
-        },
-      }))
+      .then((newUser) => {
+        const token = signToken(newUser);
+        response.status(201).send({
+          success: true,
+          token,
+          user: {
+            id: newUser.id,
+            name: newUser.name,
+            role: newUser.role,
+            email: newUser.email,
+          },
+        });
+      })
       .catch(error => response.status(500).send(error));
   }
 }
