63dc563d1f31d58154182765f7c8917a80f11841
rotimi-babalola <rotimi.babalola@andela.com>
2018-05-24 12:50:24 +0100

    remove password from response when creating a user

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -3,7 +3,15 @@ import { User } from '../models';
 class UserController {
   static create(request, response) {
     User.create(request.body)
-      .then((newUser) => response.status(201).send(newUser))
+      .then((newUser) => response.status(201).send({
+        success: true,
+        user: {
+          id: newUser.id,
+          name: newUser.name,
+          role: newUser.role,
+          email: newUser.email,
+        },
+      }))
       .catch(error => response.status(500).send(error));
   }
 }
